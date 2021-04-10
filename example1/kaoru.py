b397d8ae41b817750b7701ef5326177fd84b2744
Worawut <tonmanna@gmail.com>
2021-04-10 18:32:20 +0700

    kaoru

---
+++ example1/kaoru.py
@@ -0,0 +1,23 @@
+kaoruname="koro sensei"
+print(kaoruname)
+
+kaoru=5
+korosensei=3
+mafuyu=2
+jo=1
+buddha=4
+money=kaoru+korosensei/mafuyu*jo-buddha
+print("kaoru money:",money)
+#*/+-
+
+
+
+nani='''
+kaoro narag mak mak 
+adm love kaoru mak mak 
+why koro sensei Die 
+why nayisa kill koro sensei 
+why mafuyu dont love kanagi 
+why jo dont love cheery blossum
+'''
+print(nani)
