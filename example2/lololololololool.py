735c79250fb1627b0b72cad29cb9507443f544b0
Worawut <tonmanna@gmail.com>
2021-04-18 18:56:25 +0700

    example 3

---
+++ example2/lololololololool.py
@@ -0,0 +1,47 @@
+slime='SLIME NNNNNN'
+slime=slime.lower().capitalize()
+#print(slime)
+yaio='''
+=============================================
+yaio is good mak mak. kaoru narag mak mak
+=============================================
+=============================================
+=============================================
+============================================
+============================================
+============================================
+============================================
+============================================
+===================================================================================================================
+==================================================
+===============
+=====kaoru
+==
+'''
+yaio=yaio.capitalize().lower()
+#print(yaio)
+yaio=yaio.replace("kaoru",slime,1)
+print(yaio)
+print(yaio.count("="))
+
+
+
+
+n=1101
+f=1.010110
+s="kanagi"
+print("{1}jkjkj {0}nmnjnjk {2}".format(n,f,s))
+
+
+
+ #first_name_input = input("please enter name : ".capitalize())
+ #last_name_input = input("Please enter last name : ")
+
+ #print("First : {0} Last : {1}".format(first_name_input,last_name_input))
+
+kaoru="please enter number + please enter last number"
+print(kaoru)
+five = int(input("please enter number : " ))
+night = int(input("please enter last number : "))
+result = five + night
+print("Five :{0} ".format(result))
---
+++ example3/kokokoko.py
@@ -0,0 +1,45 @@
+ednard= '''
+========-==-=-----ddddd
+dd------=-   -=---
+-----          ddddd d   ---------------
+ =-=--   --------
+    --p-p[[[-]d]]
+ lpfdfdf.fdd=-----
+--dddddd
+[=----]==f--  ======--[   ]
+- - --------------      
+dd
+  ======= -  ---------- -  - s
+  dsfdfsfsfsd dsdsdsdddd
+----
+--------------
+-------
+'''
+oli='''
+-------------________++++++_____==-=-=
+==-=-=-=[=[oog;sg,skgwrggo=rkgkrkg;----]]
+-==-0=-p[p=-]rg
+-=-=-=-p=-0==g
+-=_+O+P{K+O}rgtrghhrrg
+-=-0-=0-0_+)_)_+plogijjgrh8rr
+=-0=-o=P+_P+_
+----=-
+p-=-=p-lfkporkgorjgiguighruighrhjiji
+'''
+print(oli)
+print(ednard)
+print(ednard.count('d'))
+print(oli.count('-'))
+
+#input
+#calculation (+,-,*,/)
+
+
+text1 = '''
+'''
+text2 = '''
+'''`
+print(text1)
+print(text2)
+print(text1.count('d'))
+print(text2.count('-'))
